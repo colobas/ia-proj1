7c6737e57fb74b7fcebf37f8c828db78da6ace73
colobas <guilherme.pg.pires@gmail.com>
2016-10-17 15:13:52 +0100

    uninformed solution

--- datastructs.py
+++ datastructs.py
@@ -14,7 +14,8 @@ class Stack(Node):
 		self.size = size
 		self.casks = []
 		self.space_left = size
-
+	def __repr__(self):
+		return "{0} {1}".format(self.id, tuple(sorted(self.casks)))
 class Exit(Node):
 	def __init__(self):
 		Node.__init__(self, 'EXIT')
@@ -47,7 +48,7 @@ class HCB:
 				cask_id = l[0]
 				cask_length = int(l[1])
 				cask_weight = float(l[2])
-				self.casks[cask_id] = Cask(cask_id, cask_length, cask_weight)
+				self.casks[cask_id] = Cask(cask_id, cask_weight, cask_length)
 
 			elif l[0][0] == 'S':
 				stack_id = l[0]
@@ -55,10 +56,11 @@ class HCB:
 				stack = Stack(stack_id, stack_size)
 				casks = l[2:]
 				for cask in casks:
-					stack.casks.append(cask[1:])
+					stack.casks.append(cask)
+					stack.space_left-= self.casks[cask].length
 
-				self.initial_state.stacks[str(stack)] = stack
-				self.nodes[str(stack)] = Stack(stack_id, stack_size)
+				self.initial_state.stacks[stack_id] = stack
+				self.nodes[stack_id] = Stack(stack_id, stack_size)
 
 			elif l[0][0] == 'E':
 				if not l[1] in self.nodes:
@@ -88,45 +90,63 @@ class HCBStateRepresentation(StateRepresentation):
 			self.hcb = hcb
 			self.setup()
 
+	def __repr__(self):
+		s = ""
+		for st in sorted(self.stacks.items()):
+			s += "{0}".format(st)
+		return "{0} {1} {2}".format(s, self.CTS_pos, self.cask_on_CTS)
+
+	def __hash__(self):
+		return hash(self.__repr__())
+
+	def __eq__(self, other):
+		return self.__hash__() == other.__hash__()
+
 	def checksol(self):
 		if self.cask_on_CTS == self.hcb.goalCask and self.CTS_pos == self.hcb.nodes['EXIT'].id:
-			return cost
+			return self.fcost
 		else:
 			return -1
 
 	def setup(self): #definir operacoes possiveis deste nó
-		if type(self.CTS_pos) == Stack and self.cask_on_CTS != None and self.prev_operation != "load": #undoing previous operation not allowed
-			if self.CTS_pos.space_left >= self.cask_on_CTS.length: # if cask fits in stack
-				fcost = self.fcost + 1 + hcb.casks[cask_on_CTS].weigh
+		if type(self.hcb.nodes[self.CTS_pos]) == Stack and self.cask_on_CTS != None and self.prev_operation != "load": #undoing previous operation not allowed
+			if self.stacks[self.CTS_pos].space_left >= self.hcb.casks[self.cask_on_CTS].length: # if cask fits in stack
+				fcost = 1 + self.hcb.casks[self.cask_on_CTS].weight
 				gcost = 0
-				self.operations["load"] = {
+				self.operations["unload"] = {
 							'function' : self.unload, 
-							'description' : "unload {0} {1} {2}".format(self.cask_on_CTS,self.CTS_pos, fcost), 
-							'fcost' : fcost,
+							'description' : "unload {0} {1} {2}".format(self.cask_on_CTS, self.CTS_pos, fcost), 
+							'fcost' : fcost + self.fcost,
 							'gcost' : gcost
 				}
 		
 		elif type(self.hcb.nodes[self.CTS_pos]) == Stack and self.cask_on_CTS == None and self.prev_operation != "unload": #undoing previous operation not allowed
-			if self.hcb.nodes[self.CTS_pos].space_left != self.hcb.nodes[self.CTS_pos].size: # if stack isn't empty
-				fcost = self.fcost + 1 + hcb.casks[cask_on_CTS].weight
+			if self.stacks[self.CTS_pos].space_left != self.stacks[self.CTS_pos].size: # if stack isn't empty
+				cask_on_CTS = self.hcb.casks[self.stacks[self.CTS_pos].casks[-1]]
+				fcost = 1 + cask_on_CTS.weight
 				gcost = 0
-				self.operations["unload"] = {
+				self.operations["load"] = {
 							'function' : self.load, 
-							'description' : "load {0} {1} {2}".format(self.cask_on_CTS, self.CTS_pos, fcost), 
-							'fcost' : fcost,
+							'description' : "load {0} {1} {2}".format(cask_on_CTS.id, self.CTS_pos, fcost), 
+							'fcost' : fcost + self.fcost,
 							'gcost' : gcost
 				}
 
 		for neighbour in self.hcb.nodes[self.CTS_pos].neighbours:
-			if ((self.parent == None) or not (neighbour == self.parent.CTS_pos) and self.prev_operation == "move"): #moving back not allowed
-				fcost = self.fcost + self.hcb.nodes[self.CTS_pos].neighbours[neighbour]
+			if (self.parent == None) or not ((neighbour == self.parent.CTS_pos) and "move" in self.prev_operation): #moving back not allowed
+				if self.cask_on_CTS == None:
+					fcost = self.hcb.nodes[self.CTS_pos].neighbours[neighbour]
+				else:
+					fcost = (1 + self.hcb.casks[self.cask_on_CTS].weight)*self.hcb.nodes[self.CTS_pos].neighbours[neighbour]
+
 				gcost = 0
-				def move_to_neighbour():
+				def move_to_neighbour(neighbour=neighbour):
 					return self.move(neighbour)
-				self.operations["move"] = {
+				move_key = "move{0}".format(neighbour)
+				self.operations[move_key]= {
 							'function' : move_to_neighbour, 
 							'description' : "move {0} {1} {2}".format(self.CTS_pos, self.hcb.nodes[neighbour].id, fcost), 
-							'fcost' : fcost,
+							'fcost' : fcost + self.fcost,
 							'gcost' : gcost
 				}
 
@@ -134,27 +154,28 @@ class HCBStateRepresentation(StateRepresentation):
 		if self.cask_on_CTS == None:
 			next_cost = self.hcb.nodes[self.CTS_pos].neighbours[to]
 		else:
-			next_cost = (1 + self.cask_on_CTS.weight)*self.CTS_pos.neighbours[to]
+			next_cost = (1 + self.hcb.casks[self.cask_on_CTS].weight)*self.hcb.nodes[self.CTS_pos].neighbours[to]
 		
+		next_cost = self.fcost + next_cost
 		next_CTS_pos = self.hcb.nodes[to].id
 
 		next_stacks = copy(self.stacks)
-		child = HCBStateRepresentation(self, self.hcb, next_cost, 0, next_stacks, next_CTS_pos, self.cask_on_CTS, "move")
+		child = HCBStateRepresentation(self, self.hcb, next_cost, 0, next_stacks, next_CTS_pos, self.cask_on_CTS, "move{0}".format(to))
 		return child
 
 	def unload(self):
-		next_cost = 1 + hcb.casks[cask_on_CTS].weight
+		next_cost = 1 + self.hcb.casks[self.cask_on_CTS].weight + self.fcost
 		next_stacks = copy(self.stacks)
 		next_CTS_pos = self.CTS_pos
 		next_stacks[self.CTS_pos].casks.append(self.cask_on_CTS)
-		next_stacks[self.CTS_pos].space_left -= hcb.nodes[cask_on_CTS].length
+		next_stacks[self.CTS_pos].space_left -= self.hcb.casks[self.cask_on_CTS].length
 		child = HCBStateRepresentation(self, self.hcb, next_cost, 0, next_stacks, self.CTS_pos, None, "unload")
 		return child
 
 	def load(self):
-		next_cost = 1 + hcb.casks[cask_on_CTS].weight
+		next_cost = 1 + self.hcb.casks[self.stacks[self.CTS_pos].casks[-1]].weight + self.fcost
 		next_stacks = copy(self.stacks)
 		next_cask_on_CTS = next_stacks[self.CTS_pos].casks.pop()
-		next_stacks[self.CTS_pos].space_left += hcb.casks[next_cask_on_CTS].length
+		next_stacks[self.CTS_pos].space_left += self.hcb.casks[next_cask_on_CTS].length
 		child = HCBStateRepresentation(self, self.hcb, next_cost, 0, next_stacks, self.CTS_pos, next_cask_on_CTS, "load")
 		return child
--- search.py
+++ search.py
@@ -9,16 +9,17 @@ class StateRepresentation:
 	def checksol(self):
 		pass
 	def backtrack_sol(self, root):
+
 		node = self
 		print_queue = []
 		while node != root:
-			print_queue.append(node.parent.operations[node.prev_operation])
+			line = node.parent.operations[node.prev_operation]['description']
+			print_queue.append(line)
+			node = node.parent
 
-		print_queue = print_queue.reverse()
-		for line in print_queue:
-			print(line)
+		print_queue.reverse()
 
-		return print_queue
+		return (print_queue, self.fcost)
 
 def insert(seq, keys, item, keyfunc):
 	k = keyfunc(item)  # get key
@@ -27,6 +28,7 @@ def insert(seq, keys, item, keyfunc):
 	seq.insert(i, item)  # insert the item itself in the corresponding spot	
 
 def uniformCost(root_state):
+	explored = []
 	queue_keys = []
 	queue = [] #queue of available moves to take, where the first to come out is the one with the smaller cost
 	
@@ -35,13 +37,23 @@ def uniformCost(root_state):
 	cur_node = root_state
 
 	while len(queue) != 0:
-		todo = queue.pop(0)
-		print(todo['description'])
-		cur_node = todo['function']()
 
+		todo = queue.pop(0)
+		queue_keys.pop(0)
+		cur_node = todo['function']()
+		print(cur_node)
+		for op in cur_node.operations:
+			print("\t"+cur_node.operations[op]['description'])
+
+		if cur_node.__hash__() in explored:
+			continue
 		if cur_node.checksol() != -1:
-			return cur_node.backtrack_sol()
+			return cur_node.backtrack_sol(root_state)
 		else:
 			for operation in cur_node.operations:
-				insert(queue, queue_keys, cur_node.operations[operation], keyfunc = lambda x: x['fcost'])
+				node = cur_node.operations[operation]['function']()
+				if not node.__hash__() in explored:
+					insert(queue, queue_keys, cur_node.operations[operation], keyfunc = lambda x: x['fcost'])
 
+		explored.append(cur_node.__hash__())
+	return
